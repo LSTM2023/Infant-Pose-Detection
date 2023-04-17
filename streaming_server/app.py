05eda8076fef70d403cc7df78eba97c91ceebf1b
Pi Sun Woo <ppssww1202@naver.com>
2023-04-17 19:33:35 +0900

    RPi_Pose Estimation_Base

---
+++ streaming_server/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, Response
+import cv2
+import time
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0) # use 0 for web camera
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 2592
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 1944
+camera.set(cv2.CAP_PROP_FPS, 60)
+
+def gen_frames(): # generate frame by frame from camera
+    prevTime = 0
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read() # read the camera frame
+        if not success:
+            break
+        else:
+            frame = cv2.flip(frame, -1)
+            
+            curTime = time.time()
+            sec = curTime - prevTime
+            prevTime = curTime
+            fps = 1. / sec
+            str = "FPS : %0.01f" % fps
+            cv2.putText(frame, str, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # concat frame one by one and show result
+
+@app.route('/video_feed')
+def video_feed():
+    # Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True)
