dc35c7666f34bb83234b003be65fe324d48f5411
Pi Sun Woo <ppssww1202@naver.com>
2023-07-19 13:58:32 +0000

    Update Library

--- server_pose_estimation/utils/coco_to_yolo.py
+++ server_pose_estimation/utils/coco_to_yolo.py
@@ -1,3 +1,3 @@
-from ultralytics.yolo.data.converter import convert_coco
+from ultralytics.data.converter import convert_coco
 
-convert_coco(labels_dir='./SyRIP_COCO/annotations/200R_1000S/', use_keypoints=True)
+convert_coco(labels_dir='./dataset/SyRIP_COCO/annotations/200R/', use_keypoints=True)
